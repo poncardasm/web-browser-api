4fbf789806ba5102b574a9553326a7afc35cbf9e
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-09-14 00:14:05 +0300

    Create 03-canvas-api project directory

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         geolocation: resolve(__dirname, '01-geolocation/index.html'),
         map_location: resolve(__dirname, `02-map-position/index.html`),
+        canvas_api: resolve(__dirname, `03-canvas-api/index.html`),
       },
     },
   },
