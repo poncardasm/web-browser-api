7741f6bddbf571f0384c443657188622c68d5fb7
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-09-18 13:05:28 +0300

    Create 06-animated-clock-2 directory

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         map_location: resolve(__dirname, `02-map-position/index.html`),
         canvas_api: resolve(__dirname, `03-canvas-api/index.html`),
         animated_clock_1: resolve(__dirname, `05-animated-clock-1/index.html`),
+        animated_clock_2: resolve(__dirname, `06-animated-clock-2/index.html`),
       },
     },
   },
