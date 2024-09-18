e472405e9c2af327096426d7f293615f44d9662c
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-09-18 13:35:29 +0300

    Refactor clock styling and add color customization options

---
+++ 06-animated-clock-2/script.js
@@ -0,0 +1,124 @@
+function clock() {
+  const now = new Date();
+
+  /**
+   * @type {HTMLCanvasElement}
+   */
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  // SETUP CANVAS
+
+  // Save the default state
+  ctx.save();
+  ctx.clearRect(0, 0, 500, 500);
+
+  // Put 0, 0 in the middle
+  ctx.translate(250, 250);
+
+  // Rotate clock -90 degrees
+  ctx.rotate(-Math.PI / 2);
+
+  // Set default styles
+  ctx.strokeStyle = '#000000';
+  ctx.lineWidth = 8;
+  ctx.fillStyle = '#f4f4f4';
+  ctx.lineCap = 'round';
+
+  // Draw clock face / border
+  ctx.save();
+
+  ctx.beginPath();
+  ctx.strokeStyle = '#444CE7';
+  ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.restore();
+
+  // Draw hour ticks
+  ctx.save();
+  for (let i = 0; i < 12; i++) {
+    ctx.beginPath();
+    ctx.rotate(Math.PI / 6);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(120, 0);
+    ctx.stroke();
+  }
+
+  ctx.restore();
+
+  // Draw minute ticks
+  ctx.save();
+  ctx.lineWidth = 4;
+  for (let i = 0; i < 60; i++) {
+    if (i % 5 !== 0) {
+      ctx.beginPath();
+      ctx.moveTo(117, 0);
+      ctx.lineTo(120, 0);
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI / 30);
+  }
+
+  ctx.restore();
+
+  // Get current time
+  const hr = now.getHours() % 12;
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+
+  console.log(`${hr}:${min}:${sec}`);
+
+  // Draw hour hand
+  ctx.save();
+
+  ctx.rotate(
+    (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+  );
+  ctx.strokeStyle = `#444CE7`;
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+
+  ctx.restore();
+
+  // Draw minute hand
+  ctx.save();
+
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.strokeStyle = `#444CE7`;
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(110, 0);
+  ctx.stroke();
+
+  ctx.restore();
+
+  // Draw second hand
+  ctx.save();
+
+  ctx.rotate((sec * Math.PI) / 30);
+  ctx.strokeStyle = `#E744E7`;
+  ctx.fillStyle = `#E744E7`;
+  ctx.lineWidth = 4;
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(110, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+  ctx.fill();
+
+  ctx.restore();
+
+  // Restore default state
+  ctx.restore();
+
+  requestAnimationFrame(clock);
+}
+
+requestAnimationFrame(clock);
