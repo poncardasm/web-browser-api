311f2b03bf8b653d6428d59ea89c18d19b779efc
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-09-25 21:10:41 +0300

    Add functionality for updating progress bar in the music player

--- 08-music-player-project/script.js
+++ 08-music-player-project/script.js
@@ -64,3 +64,10 @@ function nextSong() {
   loadSong(songs[songIndex]);
   playSong();
 }
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
