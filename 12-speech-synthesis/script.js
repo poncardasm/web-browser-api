6c3224d3caee2e1bc8802487c39af6b6de0bc79a
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-10-09 12:13:34 +0300

    feat: Implement speech synthesis functionality
    - Add event listener for form submission to trigger speech synthesis
    - Create SpeechSynthesisUtterance from text input value
    - Set selected voice for the utterance
    - Speak the utterance using the Web Speech API

--- 12-speech-synthesis/script.js
+++ 12-speech-synthesis/script.js
@@ -1,4 +1,23 @@
 const synth = window.speechSynthesis;
+const voiceSelect = document.getElementById('voice-select');
+let voices = [];
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} - ${voices[i].lang}`;
+
+    if (voices[i].default) {
+      option.textContent += ' - DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+}
 
 function onSubmit(e) {
   e.preventDefault();
@@ -6,7 +25,20 @@ function onSubmit(e) {
   const textInput = document.getElementById('text-input');
   const utterance = new SpeechSynthesisUtterance(textInput.value);
 
+  const selectedOption =
+    voiceSelect.selectedOptions[0].getAttribute('data-name');
+  for (let i = 0; i < voices.length; i++) {
+    if (voices[i].name === selectedOption) {
+      utterance.voice = voices[i];
+    }
+  }
+
   synth.speak(utterance);
+}
+
+populateVoiceList();
+if (speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
 }
 
 document.getElementById('form').addEventListener('submit', onSubmit);
